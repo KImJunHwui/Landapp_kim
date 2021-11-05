bd777a4b5c48ba2a5030b4f8694e39a3778efc1e
KJH <daeding7@naver.com>
2021-11-05 21:15:54 +0900

    방 목록 어댑터 뼈대 작업

---
+++ app/src/main/java/com/kim/landapp_kim/adapters/RoomAdapter.kt
@@ -0,0 +1,32 @@
+package com.kim.landapp_kim.adapters
+
+import android.content.Context
+import android.content.LocusId
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.kim.landapp_kim.R
+import com.kim.landapp_kim.data.Room
+
+class RoomAdapter(
+    val mContext : Context,
+    val resId : Int,
+    val mList : ArrayList<Room>) : ArrayAdapter<Room> (mContext, resId, mList){
+
+        val inflater = LayoutInflater.from(mContext)
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if(tempRow == null){
+
+            tempRow = inflater.inflate(R.layout.room_list_item,null)
+
+
+
+        }
+
+        val row = tempRow!!
+        return row
+    }
+}
