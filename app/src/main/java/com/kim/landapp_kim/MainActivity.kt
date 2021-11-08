870207aa00665e7313316a58c2413c1c27a5f5d2
KJH <daeding7@naver.com>
2021-11-08 20:38:07 +0900

    방 목록 클릭시 -> 방에 대한 정보를 들고 상세화면으로 이동. - 상세화면에서 받아서 사용 X

--- app/src/main/java/com/kim/landapp_kim/MainActivity.kt
+++ app/src/main/java/com/kim/landapp_kim/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.kim.landapp_kim
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.kim.landapp_kim.adapters.RoomAdapter
@@ -28,7 +29,16 @@ lateinit var mRoomAdapter : RoomAdapter
 
         mRoomAdapter = RoomAdapter(this,R.layout.room_list_item,mRoomList)
         roomListView.adapter = mRoomAdapter
+        
+        roomListView.setOnItemClickListener { parent, view, position, id ->
 
+            val clickedRoom = mRoomList[position]
+
+            val myIntent = Intent(this, ViewRoomDetailActivity::class.java)
+            myIntent.putExtra("room",clickedRoom)
+            startActivity(myIntent)
+
+        }
 
     }
 }
